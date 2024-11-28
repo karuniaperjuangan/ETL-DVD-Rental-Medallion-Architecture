f6ea8977b8f2c7938dae7154435e7534725eecd9
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2024-11-29 06:09:23 +0700

    fix: make fact_rental.sql join consistenct using on keyword

--- postgres_transform_dvd_rental/models/fact_rental.sql
+++ postgres_transform_dvd_rental/models/fact_rental.sql
@@ -9,9 +9,9 @@ co.country_id,
 COALESCE(DATE_PART('day',r.return_date-r.rental_date),0) AS rental_days,
 COALESCE(SUM(p.amount),0) AS total_payment_amount 
 FROM {{source('dvd_rental','rental') }} r
-LEFT JOIN {{source('dvd_rental','staff')}} s USING (staff_id)
-LEFT JOIN {{source('dvd_rental','customer')}} cu USING (customer_id)
-LEFT JOIN {{source('dvd_rental','inventory')}} i USING (inventory_id)
+LEFT JOIN {{source('dvd_rental','staff')}} s ON r.staff_id = s.staff_id
+LEFT JOIN {{source('dvd_rental','customer')}} cu ON r.customer_id = cu.customer_id
+LEFT JOIN {{source('dvd_rental','inventory')}} i ON r.inventory_id = i.inventory_id
 LEFT JOIN {{source('dvd_rental','film')}} f ON i.film_id = f.film_id
 LEFT JOIN {{source('dvd_rental','film_category')}} fc ON f.film_id = fc.film_id
 LEFT JOIN {{source('dvd_rental','category')}} cat ON fc.category_id = cat.category_id
